159af60d21ad3976423256eb001f0ebf4871cd24
fayca121 <bz.faycal@gmail.com>
2021-08-06 16:19:35 +0200

    add exception handlers on controllers

--- src/main/java/dz/bououza/controller/ConfirmationTokenController.java
+++ src/main/java/dz/bououza/controller/ConfirmationTokenController.java
@@ -9,8 +9,11 @@ import dz.bououza.service.IConfirmationTokenService;
 import dz.bououza.service.IRegistrationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("api/v1/token")
@@ -23,12 +26,19 @@ public class ConfirmationTokenController {
 
     @PostMapping
     public ResponseEntity<TokenResponse> getNewToken(@RequestBody TokenRequest tokenRequest){
-        AppUser appUser=registrationService.findUserByEmail(tokenRequest.getEmail()).get();
-        ConfirmationToken confirmationToken=confirmationTokenService.createNewToken(appUser);
-        TokenResponse tokenResponse=new TokenResponse();
-        tokenResponse.setEmail(appUser.getEmail());
-        tokenResponse.setToken(confirmationToken.getToken());
-        tokenResponse.setCreatedAt(confirmationToken.getCreatedAt());
-        return ResponseEntity.ok(tokenResponse);
+        AppUser appUser=registrationService.findUserByEmail(tokenRequest.getEmail())
+                .orElseThrow(()-> new NoSuchElementException("This email does not exist"));
+            ConfirmationToken confirmationToken = confirmationTokenService.createNewToken(appUser);
+            TokenResponse tokenResponse=new TokenResponse();
+            tokenResponse.setEmail(appUser.getEmail());
+            tokenResponse.setToken(confirmationToken.getToken());
+            tokenResponse.setCreatedAt(confirmationToken.getCreatedAt());
+            return ResponseEntity.ok(tokenResponse);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ApiResponse> NoSuchElementExceptionHandler(NoSuchElementException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(false,e.getMessage()));
     }
 }
