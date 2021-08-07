128fbd1c8fb5930795446b2b10b4a69139446840
fayca121 <bz.faycal@gmail.com>
2021-08-07 13:58:13 +0200

    add registration validation

--- src/main/java/dz/bououza/dto/RegistrationRequest.java
+++ src/main/java/dz/bououza/dto/RegistrationRequest.java
@@ -2,10 +2,20 @@ package dz.bououza.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 public class RegistrationRequest {
     private String firstName;
     private String lastName;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 6,max = 20)
     private String password;
 }
